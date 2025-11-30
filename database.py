dce63c38b1b99e363e0877245b5c3f4e45cffc67
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-11-30 19:22:35 -0300

    chore: pass echo=True to display SQL statements on terminal

--- database.py
+++ database.py
@@ -11,6 +11,6 @@ DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('DB_NAME')
 
-engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
