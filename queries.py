e22d29abebc96fc639be39f2d56fe1ca48e079c4
juanlramos <juanl.bnr@gmail.com>
2025-12-03 22:06:23 -0300

    feat: queries 5 - 9 criadas

--- queries.py
+++ queries.py
@@ -2,6 +2,7 @@ from database import SessionLocal
 from schema import Artista, Musica, MusicaPlaylist, Playlist, Usuario
 from sqlalchemy import select, func, desc
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import joinedload, aliased
 import pandas as pd
 import sys
 
@@ -73,6 +74,106 @@ def query4():
 
     query_result = session.execute(query).all()
 
+    return query_result
+
+# Query 5
+# Detalhes Completos da Música com Artista: Crie uma função para buscar uma Música por seu id e, 
+# em uma única operação de consulta (evitando o problema N+1), carregue (fetch) automaticamente
+# todos os detalhes do Artista relacionado. (Foco em Eager Loading ou Fetching Join).
+def query5(musica_id: int):
+    query = (
+        select(Musica)
+        .options(joinedload(Musica.artista)) # Eager loading do Artista associado
+        .where(Musica.id == musica_id)
+    )
+    
+    query_result = session.execute(query).scalar()
+
+    return query_result
+
+# Query 6
+# Tempo Total de Reprodução da Playlist: Para cada PLAYLIST no sistema,
+# calcule e retorne o tempo total de reprodução (soma de duracao_segundos de todas as músicas).
+# A saída deve listar o nome da Playlist, o username do Dono e o tempo total de reprodução.
+# (Foco em agregação SUM e GROUP BY sobre o N:N).
+def query6():
+    query = (
+        select(
+            Playlist.nome,
+            Usuario.username,
+            func.sum(Musica.duracao_segundos).label("tempo_total")
+        )
+        .join(Usuario, Playlist.usuario_id == Usuario.id)
+        .outerjoin(MusicaPlaylist, Playlist.playlist_id == MusicaPlaylist.playlist_id)
+        .outerjoin(Musica, MusicaPlaylist.musica_id == Musica.id)
+        .group_by(Playlist.nome, Usuario.username)
+        .order_by(desc("tempo_total"))
+    )
+
+    query_result = session.execute(query).all()
+    
+    return query_result
+
+# Query 7
+# Músicas Mais Curtas que a Média do Artista: Liste todas as Músicas cujo tempo de duração (duracao_segundos)
+# é menor que o tempo de duração médio de todas as músicas do seu próprio Artista (ex: listar músicas do AC/DC
+# que são mais curtas que a média do AC/DC). (Foco em subconsultas ou Window Functions se o ORM suportar).
+def query7():
+    MusicaInner = aliased(Musica)
+
+    # Subconsulta que Calcula a média de duração das músicas do artista da linha atual
+    media_artista_subquery = (
+        select(func.avg(MusicaInner.duracao_segundos))
+        .where(MusicaInner.artista_id == Musica.artista_id) 
+        .scalar_subquery()
+    )
+
+    query = (
+        select(
+            Musica.titulo,
+            Artista.nome.label("nome_artista"),
+            Musica.duracao_segundos,
+            media_artista_subquery.label("media_do_artista")
+        )
+        .join(Musica.artista)
+        .where(Musica.duracao_segundos < media_artista_subquery)
+        .order_by(Artista.nome)
+    )
+
+    query_result = session.execute(query).all()
+    return query_result
+
+# Query 8
+# Busca em Tabela de Junção com Atributos Extras: Liste o título de todas as Músicas na playlist 'Rock do Pablo',
+# incluindo a ordem_na_playlist de cada música.
+def query8(nome_playlist: str):
+    query = (
+        select(Musica.titulo, MusicaPlaylist.ordem_na_playlist)
+        .join(MusicaPlaylist, Musica.id == MusicaPlaylist.musica_id)
+        .join(Playlist, (MusicaPlaylist.playlist_id == Playlist.playlist_id) & 
+                        (MusicaPlaylist.usuario_id == Playlist.usuario_id))
+        .where(Playlist.nome == nome_playlist)
+        .order_by(MusicaPlaylist.ordem_na_playlist)
+    )
+    
+    query_result = session.execute(query).all()
+    return query_result
+
+# Query 9
+# Busca por Chave Composta Invertida: Encontre o username do Usuário que é o dono da Playlist
+# que contém a MUSICA 'Bohemian Rhapsody'. O filtro deve começar pela MUSICA e navegar de volta para o USUARIO.
+def query9(titulo_musica: str):
+    query = (
+        select(Usuario.username)
+        .select_from(Musica)
+        .join(MusicaPlaylist, Musica.id == MusicaPlaylist.musica_id)
+        .join(Playlist, (MusicaPlaylist.playlist_id == Playlist.playlist_id) & 
+                        (MusicaPlaylist.usuario_id == Playlist.usuario_id))
+        .join(Usuario, Playlist.usuario_id == Usuario.id)
+        .where(Musica.titulo == titulo_musica)
+    )
+
+    query_result = session.execute(query).all()
     return query_result
 
 # Query 10
@@ -170,14 +271,13 @@ if __name__ == "__main__":
         print("Exemplo: python queries.py 3")
         sys.exit(1)
 
-    escolha = sys.argv[1] # Pega o primeiro argumento após o nome do arquivo
+    escolha = sys.argv[1]
 
     result = []
     colunas = []
 
     try:
         if escolha == "1":
-            # Query 1 precisa de 1 argumento extra: username
             if len(sys.argv) < 3:
                 print("Erro: Query 1 precisa do username. Ex: python queries.py 1 'Pablo'")
             else:
@@ -186,7 +286,6 @@ if __name__ == "__main__":
                 colunas = ["Nome Playlist", "Data Criação"]
 
         elif escolha == "2":
-            # Query 2 precisa de 2 argumentos extras: username e artista
             if len(sys.argv) < 4:
                 print("Erro: Query 2 precisa de user e artista. Ex: python queries.py 2 'Josue' 'Queen'")
             else:
@@ -202,6 +301,47 @@ if __name__ == "__main__":
         elif escolha == "4":
             result = query4()
             colunas = ["id", "nome", "nacionalidade"]
+
+        elif escolha == "5":
+            if len(sys.argv) < 3:
+                print("Erro: Query 5 precisa do ID da música. Ex: python queries.py 5 1")
+            else:
+                musica_id = int(sys.argv[2])
+                musica = query5(musica_id)
+                
+                if musica:
+                    result = [{
+                        "ID": musica.id,
+                        "Música": musica.titulo,
+                        "Artista": musica.artista.nome, 
+                        "Nacionalidade do Artista": musica.artista.nacionalidade
+
+                    }]
+                else:
+                    result = []
+        elif escolha == "6":
+            result = query6()
+            colunas = ["Playlist", "Dono", "Tempo Total (s)"]
+
+        elif escolha == "7":
+            result = query7()
+            colunas = ["Música", "Artista", "Duração", "Média do Artista"]
+
+        elif escolha == "8":
+            if len(sys.argv) < 3:
+                print("Erro: Informe o nome da playlist. Ex: python queries.py 8 'Rock do Pablo'")
+            else:
+                nome_pl = sys.argv[2]
+                result = query8(nome_pl)
+                colunas = ["Música", "Ordem"]
+
+        elif escolha == "9":
+            if len(sys.argv) < 3:
+                print("Erro: Informe o título da música. Ex: python queries.py 9 'Bohemian Rhapsody'")
+            else:
+                titulo = sys.argv[2]
+                result = query9(titulo)
+                colunas = ["Dono da Playlist"]
 
         elif escolha == "10":
             result = query10()
